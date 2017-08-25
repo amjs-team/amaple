edf0e94967f3910f0e962c69419d79a7a5323f48
JOU-icejs <huzhen555@qq.com>
2017-08-25 21:03:38 +0800

    设计component

--- demo/iife/src/component/Button.js
+++ demo/iife/src/component/Button.js
@@ -1,5 +1,4 @@
-ice.install ( {
-	name : "Button"
+ice.Class ( "Button" ).extends ( ice.Component ) ( {
 
 	// init方法中使用的this.props表示父作用域的state对象
 	init : function () {
@@ -10,18 +9,22 @@ ice.install ( {
 		};
 	},
 
-	view : "",
+	view : function () {
+    	return "";
+    },
 
 	// action中定义驱动器方法，会过滤不是function的属性。方法中的this.state为init方法返回的vm对象，this.action指向此driver的封装方法
 	// 定义驱动器方法时不能使用箭头函数定义，因为这样内部的this的指向将会错误
-	action : {
-		loadStart : function () {
-			this.state.opts.btnText = this.state.loading;
-		},
-		loadEnd : function () {
-			this.state.opts.btnText = this.state.btnText;
-		}
-	},
+	action : function () {
+    	return {
+			loadStart : function () {
+				this.state.opts.btnText = this.state.loading;
+			},
+			loadEnd : function () {
+				this.state.opts.btnText = this.state.btnText;
+			}
+		};
+    },
 
 	// this.action指向此driver的封装方法
 	apply : function ( http ) {
---
+++ src/Class.js
@@ -0,0 +1,12 @@
+import { extend } from "./func/util";
+
+export default function Class ( proto ) {
+	
+}
+
+extend ( Class, {
+	extends ( superClass ) {
+    	
+    	return Class;
+    }
+} );
