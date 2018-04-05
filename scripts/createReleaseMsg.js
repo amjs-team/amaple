224566d24b64b3dd3ac631929512811632f70ded
JOU-icejs <huzhen555@qq.com>
2018-04-05 11:23:41 +0800

    refactor(release-msg): Optimize the createReleaseMsg.js

--- scripts/createReleaseMsg.js
+++ scripts/createReleaseMsg.js
@@ -1,16 +1,38 @@
 const 
+	fs = require ( "fs" ),
 	version = process.argv [ 2 ] || require ( "../package.json" ).version,
 	conventionalChangelog = require ( "conventional-changelog" ),
-	fileName = `./CHANGELOG/CHANGELOG-v${ version }.md`,
-	writeStream = require ( "fs" ).createWriteStream ( fileName );
+	changeLogDir = "./CHANGELOG",
+	fileName = `${changeLogDir}/CHANGELOG-v${ version }.md`;
 
-conventionalChangelog ( {
-	preset: "angular",
-	pkg: {
-		transform ( pkg ) {
-			pkg.version = `v${ version }`;
-			return pkg;
+// 不存在文件夹时创建文件夹
+new Promise ( ( resolve, reject ) => {
+	fs.exists ( changeLogDir, isExist => {
+		resolve ( isExist );
+	} );
+} )
+.then ( isExist => {
+	return new Promise ( ( resolve, reject ) => {
+		if ( !isExist ) {
+			fs.mkdir ( changeLogDir, () => {
+				resolve ();
+			} );
 		}
-	}
+		else {
+			resolve ();
+		}
+	} );
+} )
+.then ( () => {
+	const writeStream = require ( "fs" ).createWriteStream ( fileName );
+	conventionalChangelog ( {
+		preset: "angular",
+		pkg: {
+			transform ( pkg ) {
+				pkg.version = `v${ version }`;
+				return pkg;
+			}
+		}
 
-} ).pipe ( writeStream );
+	} ).pipe ( writeStream );
+} );
